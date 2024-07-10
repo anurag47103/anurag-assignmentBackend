a4267b0e4686855a492f4f3a584a5569a2e1a0db
anurag47103 <anuragsharma47103@gmail.com>
2024-07-10 13:38:26 +0530

    Fixed db connection for deployment.

--- db.ts
+++ db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.NODE_ENV === 'production' ? process.env.DB_HOST : process.env.DB_HOST_DEV,
+  host: process.env.NODE_ENV === 'production' ? process.env.DB_HOST_PROD : process.env.DB_HOST_DEV,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
